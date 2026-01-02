5666f6494bb2a355bfd2c24254c5e257006c7bae
Benyamin <Benyamin@BENYAMIN>
2026-01-02 15:07:17 +0330

    Add project files.

---
+++ Common/Common.Domain/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Domain
+{
+    public class BaseEntity
+    {
+
+        public long Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public BaseEntity()
+        {
+            CreationDate = DateTime.Now;
+        }
+    }
+}
